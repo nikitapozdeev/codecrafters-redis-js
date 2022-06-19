322e64f996126c110ce18de6cb8fab1f1b4add40
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-06-19 22:06:26 +0400

    pass 2st stage

--- app/main.js
+++ app/main.js
@@ -1,7 +1,19 @@
 const net = require("net");
 
 const server = net.createServer(socket => {
+  socket.on('data', (data) => {
+    const command = data.toString();
+    console.log('client sent command', command);
+    if (command === '+PING\r\n') {
+      handlePing(socket)
+    }
+  });
   socket.pipe(socket);
 });
+
+const handlePing = (socket) => {
+  socket.write('+PONG\r\n');
+  console.log('server respond with PONG');
+}
 
 server.listen(6379, '127.0.0.1');
