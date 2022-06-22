943448b22cd349fc0c63c8ef3884a20362027e95
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-06-22 22:27:27 +0400

    4st-pass

--- app/main.js
+++ app/main.js
@@ -1,10 +1,8 @@
 const net = require("net");
 
 const server = net.createServer(socket => {
-  let count = 0;
   socket.on('data', (data) => {
     const command = data.toString();
-    console.log('client sent command', command);
     handlePing(socket)
     // if (command === '+PING\r\n') {
     //   handlePing(socket)
